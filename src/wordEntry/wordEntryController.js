e7774fc25af8c4b0bd4a3813b3236138cc69dad3
Evangelia Mitsopoulou <evangelia.m@ef.com>
2016-06-07 21:33:48 +0200

    word Entry Controller : Received the mangled word through pubs

--- src/wordEntry/wordEntryController.js
+++ src/wordEntry/wordEntryController.js
@@ -1,3 +1,20 @@
-window.wordApp.controller('wordEntryController',['$scope', function($scope){
+ window.wordAp.wordEntryController = function($scope, WordsService,pubsub){
+	$scope.model = {};
+
+	$scope.init = function(){
+		pubsub.addListener("wordsListReceived", $scope, onGetFirstMangledHandler);
+	};
+   
 	$scope.submit = function(){};
-}]);
+
+	$scope.refresh = function(){
+       WordsService.getRandomWord();
+	};
+
+    function onGetFirstMangledHandler(data){
+       $scope.model.mangledWord = data;
+    }
+};
+
+window.wordApp.homeController.$inject = ['$scope', 'WordsService', 'pubsubProvider'];
+window.wordApp.controller('wordEntryController', window.wordAp.wordEntryController);
