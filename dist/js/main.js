e955dc5c15823db060df3df659eb58f15a20c750
Evangelia Mitsopoulou <evangelia.m@ef.com>
2016-06-08 21:00:01 +0200

    Added Counter

--- dist/js/main.js
+++ dist/js/main.js
@@ -147,14 +147,32 @@ window.wordApp.factory("pubsubProvider", window.wordApp.pubsubProvider);
  window.wordApp.wordEntryController = function($scope, WordsService,pubsub){
 	$scope.model = {show : false};
 
+	function startTimer(duration, display) {
+    	var timer = duration, minutes, seconds;
+   			 setInterval(function () {
+       		 minutes = parseInt(timer / 60, 10);
+       		 seconds = parseInt(timer % 60, 10);
+        	 minutes = minutes < 10 ? "0" + minutes : minutes;
+        	 seconds = seconds < 10 ? "0" + seconds : seconds;
+             display.textContent =  "00:" + seconds;
+        if (--timer < 0) {
+            timer = duration;
+       	 }
+    	}, 1000);
+	}
+
      var onGetFirstMangledHandler = function(data){
        $scope.model.mangledWord = data;
        $scope.model.show = true;
+
+        //start the counter
+	    var fortySeconds = 40,
+        display = document.querySelector('#counter');
+    	startTimer(fortySeconds, display);
        
     };
 
 	$scope.init = function(){
-		console.log('on');
 		pubsub.addListener("firstMangledWord", $scope, onGetFirstMangledHandler);
 	};
    
